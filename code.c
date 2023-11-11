1387c482ae1267ae9d9b48c2c70380b3cb4fc896
noam-par <148979467+noam-par@users.noreply.github.com>
2023-11-11 15:50:59 +0200

    dry of ex1

--- code.c
+++ code.c
@@ -1,6 +1,15 @@
-includ <stdio.h>
-
-int main{
-  printf("hi i'm noam");
-  return 0;
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+char* stringDuplicator(char* s, int times){
+  assert(s);
+  assert(times > 0);
+  int len = strlen(s);
+  char* out = malloc((1+len*times)*sizeof(char));
+  assert(out);
+  for (int i=0; i<times; i++){
+    strcpy(out,s);
+    out = out + len;
+  }
+  return out - (len*times);
 }
